1a1c923e9b1ba322330d4462c3b7f6cc5597e038
Mesaque Francisco <mesaquenf@gmail.com>
2019-11-16 19:29:22 -0300

    ADD: EpisodeList test

---
+++ src/component/episode-list/__tests__/EpisodeList.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import EpisodeList from '../EpisodeList';
+/**
+ * @author Mesaque Francisco <mesaquenf@gmail.com>
+ * @since 2019-11-16
+ */
+
+describe('Label', () => {
+  let component = Renderer.create(<EpisodeList ready={false} />);
+  it('should have same snapshot when loading', () => {
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  describe('when rendering values', () => {
+    const episodes = [
+      { id: 1, episode: 'S01E01', name: 'one' },
+      { id: 2, episode: 'S01E02', name: 'two' },
+    ];
+    beforeEach(() => {
+      component = Renderer.create(<EpisodeList episodes={episodes} ready />);
+    });
+    it('should have same snapshot', () => {
+      expect(component.toJSON()).toMatchSnapshot();
+    });
+  });
+});
